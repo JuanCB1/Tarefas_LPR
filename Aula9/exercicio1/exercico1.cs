448be8f1e62b78c6a49176b7e9b68d639aaaafd7
JuanCB1 <juancarlosashcb@gmail.com>
2025-07-19 15:33:42 -0300

    ✨ class tasks 6

---
+++ Aula9/exercicio1/exercico1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    class Nome
+    {
+        public string Texto { get; set; }
+        public int Tamanho { get { return Texto.Length; } }
+
+        public Nome(string texto)
+        {
+            Texto = texto;
+        }
+    }
+
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("-------------");
+        Console.WriteLine(" Exercício 1 ");
+        Console.WriteLine("-------------");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.Write("Insira a quantidade de nomes na lista");
+        Console.ResetColor();
+        int quantidade = int.Parse(Console.ReadLine());
+        List<Nome> nomes = new List<Nome>();
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write($"Nome {i + 1}: ");
+            Console.ResetColor();
+            nomes.Add(new Nome(Console.ReadLine()));
+        }
+
+        List<Nome> nomesRestantes = new List<Nome>(nomes);
+
+        while (nomesRestantes.Count > 0)
+        {
+            List<int> tamanhosUsados = new List<int>();
+            List<Nome> linha = new List<Nome>();
+
+            for (int i = 0; i < nomesRestantes.Count; i++)
+            {
+                var nome = nomesRestantes[i];
+
+                if (!tamanhosUsados.Contains(nome.Tamanho))
+                {
+                    linha.Add(nome);
+                    tamanhosUsados.Add(nome.Tamanho);
+                    nomesRestantes.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("--------------------------");
+            Console.WriteLine(" Lista em ordem crescente ");
+            Console.WriteLine("--------------------------");
+            Console.ResetColor();
+            
+            linha = linha.OrderBy(n => n.Tamanho).ToList();
+            Console.WriteLine(string.Join(" , ", linha.Select(n => n.Texto)));
+        }
+    }
+}
---
+++ Aula9/exercicio2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+
+#define Yellow  "\033[33m"
+#define Red     "\033[31m"
+#define Blue    "\033[34m"
+#define Reset   "\033[0m"
+
+using namespace std;
+
+int main()
+{
+    cout << Blue << "-----------------" << Reset << endl;
+    cout << Blue << "   Exercício 1   " << Reset << endl;
+    cout << Blue << "-----------------" << Reset << endl;
+
+    vector<int> numeros;
+
+    srand(time(0));
+    for (int i = 0; i < 100; i++)
+    {
+        numeros.push_back(rand() % 100);
+    }
+
+    sort(numeros.begin(), numeros.end());
+
+    cout << Yellow << "Lista ordenada:\n" << Reset;
+    for (int n : numeros)
+    {
+        cout << n << " ";
+    }
+    cout << "\n\n";
+
+    list<int> listaImpares;
+    for (int n : numeros)
+    {
+        if (n % 2 != 0)
+            listaImpares.push_back(n);
+    }
+
+    cout << Blue << "Lista sem números pares:\n" << Reset;
+    for (int n : listaImpares)
+    {
+        cout << n << " ";
+    }
+    cout << "\n\n";
+
+    cout << Red << "Números repetidos:\n" << Reset;
+    bool temRepetido = false;
+    for (size_t i = 1; i < numeros.size(); i++)
+    {
+        if (numeros[i] == numeros[i - 1])
+        {
+            if (i == 1 || numeros[i] != numeros[i - 2])
+            {
+                cout << numeros[i] << " ";
+                temRepetido = true;
+            }
+        }
+    }
+
+    if (!temRepetido)
+    {
+        cout << Red << "Nenhum número repetido." << Reset;
+    }
+
+    return 0;
+}
