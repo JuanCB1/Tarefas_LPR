39646d1f73ea89d0b6b4f450b5aec1116efb33af
JuanCB1 <juancarlosashcb@gmail.com>
2025-08-31 15:58:41 -0300

    ✨ class tasks 7

---
+++ Aula10/Execicio1/Exercicio1.cs
@@ -0,0 +1,51 @@
+﻿/*Exercício 1)
+a) Fazer um programa para ler os valores
+da largura e altura de um retângulo. Em
+seguida, mostrar na tela o valor de sua
+área e perímetro.
+Usar a seguinte classe:
+---------------
+Retangulo
+---------------
+double Altura
+double Largura
+---------------
+calcArea()
+calcPerimetro()
+*/
+using System;
+
+class Retangulo
+{
+    public double Altura { get; set; }
+    public double Largura { get; set; }
+
+    public double CalcArea()
+    {
+        return Altura * Largura;
+    }
+
+    public double CalcPerimetro()
+    {
+        return 2 * (Altura + Largura);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Retangulo ret = new Retangulo();
+
+        Console.Write("Digite a largura do retângulo: ");
+        ret.Largura = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura do retângulo: ");
+        ret.Altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nÁrea do retângulo: {ret.CalcArea()}");
+        Console.WriteLine($"Perímetro do retângulo: {ret.CalcPerimetro()}");
+
+        Console.ReadKey();
+    }
+}
---
+++ Aula10/Exercicio2/Exercicio2.cs
@@ -0,0 +1,81 @@
+﻿/*Exercício 2)
+A. Fazer um programa para ler os dados
+de um funcionário (nome, salário bruto
+e imposto).
+
+B. A classe funcionário deve conter os
+seguintes métodos:
+- Mostrar os dados do funcionário (nome e
+salário líquido).
+- Aumentar o salário do funcionário com base
+em uma porcentagem dada
+
+C. OBS: Tabela de Imposto
+- 10% para salários de até 2000
+- 15% para salários de até 3000
+- 20% para salários superiores a 3000
+*/
+
+using System;
+
+class Funcionario
+{
+    public string Nome { get; set; }
+    public double SalarioBruto { get; set; }
+    public double Imposto { get; private set; }
+
+    // Calcula o imposto com base na tabela
+    public void CalcularImposto()
+    {
+        if (SalarioBruto <= 2000)
+            Imposto = 0.10 * SalarioBruto;
+        else if (SalarioBruto <= 3000)
+            Imposto = 0.15 * SalarioBruto;
+        else
+            Imposto = 0.20 * SalarioBruto;
+    }
+
+    public double SalarioLiquido()
+    {
+        return SalarioBruto - Imposto;
+    }
+
+    // Aumenta o salário com base na porcentagem informada
+    public void AumentarSalario(double percentual)
+    {
+        SalarioBruto += SalarioBruto * (percentual / 100.0);
+        CalcularImposto(); 
+    }
+
+    public void MostrarDados()
+    {
+        Console.WriteLine($"\nNome: {Nome}");
+        Console.WriteLine($"Salário líquido: {SalarioLiquido():F2}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Funcionario func = new Funcionario();
+
+        Console.Write("Digite o nome do funcionário: ");
+        func.Nome = Console.ReadLine();
+
+        Console.Write("Digite o salário bruto: ");
+        func.SalarioBruto = double.Parse(Console.ReadLine());
+
+        func.CalcularImposto();
+        func.MostrarDados();
+
+        Console.Write("\nDigite a porcentagem de aumento: ");
+        double aumento = double.Parse(Console.ReadLine());
+
+        func.AumentarSalario(aumento);
+        Console.WriteLine("\nApós aumento:");
+        func.MostrarDados();
+
+        Console.ReadKey();
+    }
+}
